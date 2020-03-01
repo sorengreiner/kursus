955304e5b3968e04712ab06dc490b4f0636edfa2
Greiner <soren@greiner.dk>
2020-03-01 21:10:17 +0100

    added zoom

--- mandelbrot.cs
+++ mandelbrot.cs
@@ -5,9 +5,14 @@ using System.Windows.Forms;
 
 public class MyForm : System.Windows.Forms.Form
 {
+    Bitmap myBitmap;
+    Pen blackPen;
+    double zoom = 4.0f;
+    double uc = 0.0f;
+    double vc = 0.0f;
     int image_width = 512;
     int image_height = 512;
-    Bitmap myBitmap;
+
     private System.Windows.Forms.PictureBox pictureBox1;
 
     private System.ComponentModel.IContainer components = null;
@@ -35,6 +40,7 @@ public class MyForm : System.Windows.Forms.Form
         this.pictureBox1.TabIndex = 0;
         this.pictureBox1.TabStop = false;
         this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+        this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
         // 
         // Form1
         // 
@@ -49,54 +55,81 @@ public class MyForm : System.Windows.Forms.Form
 
         this.Text = "Mandelbrot by Soren Greiner";
 
+        blackPen = new Pen(Color.White, 1);
         myBitmap = new Bitmap(image_width, image_height, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+
+        Render();
+    }
+
+    private void pictureBox1_Paint(object sender, PaintEventArgs e)
+    {
+        Graphics g = e.Graphics;
+        g.DrawImage(myBitmap, 0, 0, myBitmap.Width, myBitmap.Height);
+        Refresh();
+    }
+
+    private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+    {
+        MouseEventArgs me = (MouseEventArgs)e;
+        Point coordinates = me.Location;
 
         int h = myBitmap.Height;
         int w = myBitmap.Width;
+        if(e.Button == MouseButtons.Right)
+        {
+            zoom = zoom * 2f;
+        }
+        else
+        {
+            zoom = zoom / 2f;
+            uc = ((double)coordinates.X / w) * zoom - zoom / 2f + uc;
+            vc = ((double)coordinates.Y / h) * zoom - zoom / 2f + vc;
+        }
+        Render();
 
-        float zoom = 0.5f;
+        this.Invalidate();
+    }
+ 
+    private void Render()
+    {
+        int h = myBitmap.Height;
+        int w = myBitmap.Width;
+
         int red = 0;
         int green = 0;
         int blue = 0;
 
         float r0 = 0;
         float g0 = 0;
-        float b0 = 50;
+        float b0 = 40;
 
-        float r1 = 255;
-        float g1 = 200;
+        float r1 = 200;
+        float g1 = 180;
         float b1 = 0;
 
         for (int y = 0; y < h; y++)
         {
             for (int x = 0; x < w; x++)
             {
-                float u = ((float)x) / w + 0.2f;
-                float v = ((float)y) / h + 0.2f;
-                u = u * zoom;
-                v = v * zoom;
-                float cr = u * 4.0f - 2.0f;
-                float ci = v * 4.0f - 2.0f;
-                float zr = 0;
-                float zi = 0;
-
-                // perform square
-                // (zr + zi*i)*(zr + zi*i) = zr*zr - zi*zi + 2*zi*i
-                // zr = zr*zr - zi*zi
-                // zi = 2*zi
+                double u = (((double)x) / w) * zoom - zoom / 2f + uc;
+                double v = (((double)y) / h) * zoom - zoom / 2f + vc;
+                double cr = u;
+                double ci = v;
+                double zr = 0;
+                double zi = 0;
 
                 int i = 0;
                 while (true)
                 {
-                    float zr_n = zr * zr - zi * zi + cr;
-                    float zi_n = 2 * zi * zr + ci;
+                    double zr_n = zr * zr - zi * zi + cr;
+                    double zi_n = 2 * zi * zr + ci;
                     zr = zr_n;
                     zi = zi_n;
 
-                    float m = zr * zr + zi * zi;
+                    double m = zr * zr + zi * zi;
                     if (m > 4)
                     {
-                        float t = ((float)(i % 32)) / 31;
+                        float t = ((float)(i % 64)) / 63;
                         red = (int)((r1 - r0) * t + r0);
                         green = (int)((g1 - g0) * t + g0);
                         blue = (int)((b1 - b0) * t + b0);
@@ -117,16 +150,9 @@ public class MyForm : System.Windows.Forms.Form
         }
     }
 
-    private void pictureBox1_Paint(object sender, PaintEventArgs e)
-    {
-        Graphics g = e.Graphics;
-        g.DrawImage(myBitmap, 0, 0, myBitmap.Width, myBitmap.Height);
-    }
-
     public static void Main()
     {
         System.Windows.Forms.Application.Run(new MyForm());
     }
- 
 }
 
